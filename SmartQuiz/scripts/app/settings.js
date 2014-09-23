4941e55b23c2749413cc3c03624f26bf82d43482
Mijael Vargas <mijael.vargas@live.com>
2014-09-23 18:31:52 -0500

    Initial Project based on Friends Template.

--- SmartQuiz/scripts/app/settings.js
+++ SmartQuiz/scripts/app/settings.js
@@ -5,7 +5,7 @@
 var appSettings = {
 
     everlive: {
-        apiKey: '$EVERLIVE_API_KEY$', // Put your Backend Services API key here
+        apiKey: 'UrLrgLwpJgo1AMZ6', // Put your Backend Services API key here
         scheme: 'http'
     },
 
